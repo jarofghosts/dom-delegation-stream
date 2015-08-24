4b1ef68585c20dbd7bc6a752306a2741f8c9f9a7
jesse keane <me@jessekeane.me>
2015-08-24 15:43:03 -0700

    add delegationTarget

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var test = require('tape')
 
 var events = require('../')
 
-test('streams delegated dom events', function(t) {
+test('streams delegated dom events', function (t) {
   t.plan(1)
 
   var el = document.createElement('div')
@@ -15,14 +15,14 @@ test('streams delegated dom events', function(t) {
 
   el.appendChild(target)
 
-  stream.on('data', function(ev) {
+  stream.on('data', function (ev) {
     t.equal(ev.target, target)
   })
 
   target.click()
 })
 
-test('streams direct dom events', function(t) {
+test('streams direct dom events', function (t) {
   t.plan(1)
 
   var el = document.createElement('div')
@@ -35,14 +35,14 @@ test('streams direct dom events', function(t) {
 
   el.appendChild(target)
 
-  stream.on('data', function(ev) {
+  stream.on('data', function (ev) {
     t.equal(ev.target, target)
   })
 
   target.click()
 })
 
-test('stops propagation if specified', function(t) {
+test('stops propagation if specified', function (t) {
   t.plan(1)
 
   var el = document.createElement('div')
@@ -56,19 +56,66 @@ test('stops propagation if specified', function(t) {
 
   el.appendChild(target)
 
-  directStream.once('data', function(ev) {
+  directStream.once('data', function (ev) {
     t.equal(ev.target, target)
     t.end()
   })
 
-  delegateStream.once('data', function() {
+  delegateStream.once('data', function () {
     t.fail('should not be triggered')
   })
 
   target.click()
 })
 
-test('`.end()` removes listeners', function(t) {
+test('prevents default if specified', function (t) {
+  t.plan(1)
+
+  var el = document.createElement('div')
+  var target = document.createElement('input')
+  var directStream = events(target, 'click', {preventDefault: true})
+
+  document.body.appendChild(el)
+
+  target.type = 'button'
+
+  el.appendChild(target)
+
+  directStream.once('data', function (ev) {
+    t.equal(ev.defaultPrevented, true)
+    t.end()
+  })
+
+  target.click()
+})
+
+test('provides delegationTarget', function (t) {
+  t.plan(1)
+
+  var el = document.createElement('div')
+  var target = document.createElement('a')
+  var dummyTarget = document.createElement('span')
+  var directStream = events(target, 'click', '[rel=real-target]')
+
+  dummyTarget.textContent = 'dummy'
+  target.setAttribute('rel', 'real-target')
+
+  target.appendChild(dummyTarget)
+  el.appendChild(target)
+
+  document.body.appendChild(el)
+
+  el.appendChild(target)
+
+  directStream.once('data', function (ev) {
+    t.equal(ev.delegationTarget, target)
+    t.end()
+  })
+
+  dummyTarget.click()
+})
+
+test('`.end()` removes listeners', function (t) {
   t.plan(1)
 
   var el = document.createElement('div')
@@ -82,11 +129,11 @@ test('`.end()` removes listeners', function(t) {
 
   el.appendChild(target)
 
-  directStream.once('data', function() {
+  directStream.once('data', function () {
     t.fail('should not be triggered')
   })
 
-  delegateStream.once('data', function() {
+  delegateStream.once('data', function () {
     t.ok(true, 'should be triggered')
     t.end()
   })
